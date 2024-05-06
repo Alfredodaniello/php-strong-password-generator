e2933261b1260c7941d66a6d1ea8e281565ef59d
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-06 09:37:21 +0200

    milestone 1 completed

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,12 +19,25 @@
         if (isset($_GET['length'])) {
             if (is_numeric($_GET['length']) && intval($_GET['length']) >= 4) {
                 $passLength = intval($_GET['length']);
+                echo "<p>La tua password è: " . generatePass($passLength) . "</p>";
             } else {
                 echo "<p>Devi inserire un numero che sia maggiore di 3.</p>";
             }
-        }
-        var_dump($passLength);
+        };
+
+        function  generatePass($length) {
+            $password= "";
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+,-./:;<=>?@[]^_{|}~";
+            $charsLength = strlen($chars);
+            for ($i=0; $i < $length; $i++) {
+                $password .= $chars[rand(0, $charsLength - 1)];
+                }
+            return $password;
+        };
+     $password = generatePass($passLength);
+     var_dump($password)
 ?> 
+
 
 </body>
 </html>
